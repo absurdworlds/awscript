5409ae6a5a385b09af255ef84003bb7c8814d31b
Hudd <haddayn@gmail.com>
2015-04-06 13:17:44 +0300

    AST: BinaryExpr: Add Operator enum stub

--- include/daedalus/ast/BinaryExpr.h
+++ include/daedalus/ast/BinaryExpr.h
@@ -6,25 +6,31 @@
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
-#ifndef _daedalus_AST_AssignmentExpr_
-#define _daedalus_AST_AssignmentExpr_
+#ifndef _daedalus_AST_BinaryExpr_
+#define _daedalus_AST_BinaryExpr_
 #include <daedalus/ast/Expression.h>
 
 namespace daedalus {
 namespace ast {
+enum class Operator {
+
+};
+
 class BinaryExpr : public Expression {
 public:
 	virtual ~BinaryExpr();
 
 	virtual Expression* getLHS();
 	virtual Expression* getRHS();
+	virtual Operator getOperation();
 
 	virtual accept(ast::Visitor& visitor);
 private:
 	Expression* LHS;
 	Expression* RHS;
+	Operator op;
 };
 
 } // namespace ast
 } // namespace daedalus
-#endif//_daedalus_AST_AssignmentExpr_
+#endif//_daedalus_AST_BinaryExpr_
