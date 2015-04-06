4d74e6a1b3239f29f66fc301041a3e175332be80
Hudd <haddayn@gmail.com>
2015-04-06 09:37:05 +0300

    AST: Add ClassDeclaration

--- include/daedalus/ast/ClassDeclaration.h
+++ include/daedalus/ast/ClassDeclaration.h
@@ -6,12 +6,35 @@
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
+#ifndef _daedalus_AST_ClassDeclaration_
+#define _daedalus_AST_ClassDeclaration_
+#include <daedalus/ast/Declaration.h>
 namespace daedalus {
-#include <daedalus/AST/Declaration.h>
 class ClassDeclaration : public Declaration {
 public:
 	virtual ~ClassDeclaration();
-	// TODO
+
+	virtual accept(ast::Visitor& visitor);
+protected:
+	std::vector<Declaration*> body;
 };
 
+class PrototypeDeclaration : public ClassDeclaration {
+public:
+	virtual ~ClassDeclaration();
+
+	virtual accept(ast::Visitor& visitor);
+protected:
+	// It seems like prototype allows only assignments inside it,
+	// while instance additionally allows at least function calls
+	std::vector<Statement*> stmts;
+};
+
+class InstanceDeclaration : public PrototypeDeclaration {
+public:
+	virtual ~ClassDeclaration();
+
+	virtual accept(ast::Visitor& visitor);
+};
 } // namespace daedalus
+#endif//_daedalus_AST_ClassDeclaration_
