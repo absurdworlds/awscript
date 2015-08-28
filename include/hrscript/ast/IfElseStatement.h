23bb1575c13b9226f5651f237ad1b69bdaec8040
Hedede <hededrk@gmail.com>
2015-08-28 17:23:14 +0300

    AST: Update If/IfElseStatement
    - Merged IfStatement and IfElseStatement (there's no point to have both)
    - Changed file name to the class name
    - Added constructor and accept
    - Renamed class members

--- include/hrscript/ast/IfElseStatement.h
+++ include/hrscript/ast/IfElseStatement.h
@@ -8,32 +8,25 @@
  */
 #ifndef _hrscript_AST_Conditional_
 #define _hrscript_AST_Conditional_
-#include <hrscript/ast/Node.h>
+#include <hrscript/ast/Statement.h>
 
 namespace hrscript {
 namespace ast {
 class Exression;
-class Statement;
 
-class IfStatement : public Statement {
+class IfElseStatement : public Statement {
 public:
-	virtual ~IfStatement();
+	IfElseStatement(Expression* ifExpr, Statement* ifBody Statement* elseBody)
+		: ifExpr(ifExpr), ifBody(ifBody), elseBody(elseBody)
 
-	virtual accept(ast::Visitor& visitor);
+	virtual accept(ast::Visitor& visitor)
+	{
+		visitor.visit(*this)
+	}
 protected:
-	Expression* condExpr;
-	Statement* thenBlock;
-};
-
-class IfElseStatement : public IfElseStatement {
-public:
-	virtual ~IfElseStatement();
-
-	virtual accept(ast::Visitor& visitor);
-protected:
-	Expression* condExpr;
-	Statement* thenBlock;
-	Statement* elseBlock;
+	Expression* ifExpr;
+	Statement*  ifBody;
+	Statement*  elseBody;
 };
 
 } // namespace ast
