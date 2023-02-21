3379f1e3eb6238571555076fbe4e4657ff9c4524
Hudd <haddayn@gmail.com>
2023-02-21 11:43:52 +0400

    Move everything into libs

---
+++ libs/lang/include/aw/script/ast/decl/variable.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 Hudd <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_script_ast_decl_variable_h
+#define aw_script_ast_decl_variable_h
+
+#include <aw/script/ast/declaration.h>
+
+namespace aw::script::ast {
+class type;
+
+enum class access {
+	variable,
+	constant
+};
+
+class variable : public declaration {
+public:
+	variable(string_view name, access access)
+		: declaration(decl_kind::variable, name)
+		, access(access)
+	{
+	}
+
+
+	ast::type* type = nullptr;
+	ast::access access;
+};
+
+} // namespace aw::script::ast
+
+#endif // aw_script_ast_decl_variable_h
