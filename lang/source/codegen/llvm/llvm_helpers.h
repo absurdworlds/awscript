0d4da1be0c4f9d9f7040d55e3d838840d9836c8d
Hudd <haddayn@gmail.com>
2023-02-24 13:10:18 +0400

    Add missing file llvm_helpers.h

---
+++ lang/source/codegen/llvm/llvm_helpers.h
@@ -0,0 +1,28 @@
+#ifndef llvm_helpers_h
+#define llvm_helpers_h
+
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Instructions.h>
+
+namespace aw::script {
+
+/// CreateEntryBlockAlloca - Create an alloca instruction in the entry block of
+/// the function.  This is used for mutable variables etc.
+inline auto CreateEntryBlockAlloca(
+	llvm::LLVMContext& context,
+	llvm::Function* TheFunction,
+	llvm::Type* type,
+	llvm::StringRef VarName)
+		-> llvm::AllocaInst*
+{
+	auto& entry_block = TheFunction->getEntryBlock();
+
+	llvm::IRBuilder<> builder(&entry_block, entry_block.begin());
+	return builder.CreateAlloca(type, nullptr, VarName);
+}
+
+}
+
+#endif // llvm_helpers_h
