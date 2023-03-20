c62a0ec021323cea64da0ef61d39bef39be457a7
Hudd <haddayn@gmail.com>
2023-03-21 02:53:01 +0400

    Update playground

--- data/playground.d
+++ data/playground.d
@@ -1,15 +1,35 @@
-func test_function(var int x, var int y) : int
+function putchar(var int ch)
+function puts(var cstring str)
+function printf(var cstring str, var int v)
+
+function print(var cstring a, var cstring b)
+{
+	puts(a);
+	puts(b);
+}
+
+function test_function(var int x, var int y) : int
 {
 	print("Hello, ", "world!");
 
-	if (true)
+	if (x>y)
 	{
-		x = 1;
+		x = y-1;
 	}
 	else
 	{
-		y = x = 2+3*4;
+		//y = x = 2+3*4;
+		y = x*2+3*4;
 	}
 
-	-3;
+	printf("x:%d\n", x);
+	printf("y:%d\n", y);
+
+	return -3;
+}
+
+
+function main()
+{
+	return test_function(5, 10);
 }
