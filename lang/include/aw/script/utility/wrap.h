c74c9337829784c39f54c440d3d9c3b6c635dc00
Hudd <haddayn@gmail.com>
2023-03-26 04:07:50 +0400

    utility: add wrap()
    Convenient utility for converting values into unique_ptrs

---
+++ lang/include/aw/script/utility/wrap.h
@@ -0,0 +1,16 @@
+#ifndef aw_utility_wrap_h
+#define aw_utility_wrap_h
+
+#include <memory>
+
+namespace aw::script {
+
+template<typename Value, typename T = std::remove_reference_t<Value>>
+auto wrap(Value&& init) -> std::unique_ptr<T>
+{
+	return std::make_unique(std::forward<Value>(init));
+}
+
+} // namespace aw::script
+
+#endif // aw_utility_wrap_h
