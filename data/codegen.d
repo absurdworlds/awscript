97aa5503ada423675f68828618191764e5698d3c
Hudd <haddayn@gmail.com>
2023-04-05 04:28:44 +0400

    test: reshuffle the codegen.d sample

--- data/codegen.d
+++ data/codegen.d
@@ -2,15 +2,32 @@ function putchar(var ch: int);
 function puts(var str: cstring);
 function printf(var str: cstring, var v: int);
 
+var double: double = 4.0;
+var global: int = add(5,2);
+
+const else: int = 0;
+const if: int = 1;
+
+function main(var argc: int) : int
+{
+	if (else < if) {
+		hello();
+
+		printf("argc = %d\n", argc);
+		printf("div = %d\n", div(global,1));
+
+		add(66, 67);
+
+		test_reassign(argc);
+	}
+	return 0;
+}
 
 function add(var x: int, var y: int) : int
 {
 	printf("x + y = %d\n", x+y)
 	return x+y
 }
-
-var double:double = 4.0;
-var global:int = add(5,2);
 
 function div(var x: int, var y: int) : int
 	return if (y < 1)
@@ -32,20 +49,6 @@ function test_reassign(var c: int)
 
 	c=65
 	putchar(c)
-}
-
-function main(var argc: int) : int
-{
-	hello();
-
-	printf("argc = %d\n", argc);
-	printf("div = %d\n", div(global,1));
-
-	add(66, 67);
-
-	test_reassign(argc);
-
-	return 0;
 }
 
 function hello()
